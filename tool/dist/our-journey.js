417854758d36b9ef4ad8bb5621b363fa0c430b25
timcoughlan <40237215+timcoughlan@users.noreply.github.com>
2018-09-19 10:06:40 +0100

    new dist

--- tool/dist/our-journey.js
+++ tool/dist/our-journey.js
@@ -1,7 +1,7 @@
 /*!
   our-journey | 1.2.1
   © 2018 The Open University (IET) | Tim Coughlan {lead}, Glen Darby, Nick Freear | PROPRIETARY.
-  Build: 2018-09-19T08:39Z
+  Build: 2018-09-19T08:40Z
   https://github.com/IET-OU/our-journey.git
 
 */
