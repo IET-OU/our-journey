db0c727e8760b6593d67f651d7e5fdfb2ac6940d
timcoughlan <40237215+timcoughlan@users.noreply.github.com>
2018-10-29 15:54:50 +0000

    editor presentation and help text changes

--- docs/js/site.js
+++ docs/js/site.js
@@ -10,7 +10,7 @@
     '<p class="footer-copy">',
     '&copy; <a href="https://www.open.ac.uk/">The Open University</a>',
     // ' | <a rel="license" href="https://gnu.org/licenses/gpl-3.0.en.html" title="GNU General Public License [GPL-3.0+]">License</a>',
-    ' | <a class="p" href="privacy-ou-generic.html" title="Privacy policy, and terms of service">Privacy</a>',
+    ' | <a class="p" href="privacy-ou-generic.html" target="_blank" title="Privacy policy, and terms of service (opens in new window)">Privacy (opens in new window)</a>',
     '</p>',
     '<p class="footer-logos">',
     '<a href="https://iet.open.ac.uk/"><img src="assets/iet-logo.svg" alt="Institute of Educational Technology" title="Institute of Educational Technology" /></a>',
